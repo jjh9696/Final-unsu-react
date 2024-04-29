e2138dcca854412850cd61f59d4f4f3e766eedb2
simcat13 <rycbar0@gmail.com>
2024-04-29 21:24:51 +0900

    로그인구현중

--- unsu/src/App.js
+++ unsu/src/App.js
@@ -4,12 +4,47 @@ import Header from './components/Header';
 import Home from './components/Home';
 import SideBar from './components/SideBar';
 import { Route, Routes } from 'react-router';
+import { useRecoilState, useRecoilValue } from "recoil";
 import { Suspense, lazy, useCallback, useEffect } from 'react';
+import { loginIdState, loginLevelState, isLoginState } from './components/utils/RecoilData';
 import LoadingScreen from './components/LoadingScreen';
+import axios from './components/utils/CustomAxios';
+
+// 컴포넌트 배치
 import Login from './components/integrated/Login';
 import Notice from './components/integrated/Notice';
 
 const App = () => {
+
+  // recoil state
+  const [loginId, setLoginId] = useRecoilState(loginIdState);
+  const [loginLevel, setLoginLevel] = useRecoilState(loginLevelState);
+
+  // recoil value
+  const isLogin = useRecoilValue(isLoginState);
+
+  //effect
+  useEffect(() => {
+    refreshLogin();
+  }, []);//최초 1회
+
+  //callback
+  const refreshLogin = useCallback(async () => {
+    //localStorage에 있는 refreshToken의 유무에 따라 로그인 처리를 수행
+    const refreshToken = window.localStorage.getItem("refreshToken");
+    //console.log(refreshToken);
+    if (refreshToken !== null) {//refreshToken 항목이 존재한다면
+      //리프레시 토큰으로 Authorization을 변경하고
+      axios.defaults.headers.common["Authorization"] = refreshToken;
+      //재로그인 요청을 보낸다
+      const resp = await axios.post("/member/refresh");
+      //결과를 적절한 위치에 설정한다
+      setLoginId(resp.data.memberId);
+      setLoginLevel(resp.data.memberLevel);
+      axios.defaults.headers.common["Authorization"] = resp.data.accessToken;
+      window.localStorage.setItem("refreshToken", resp.data.refreshToken);
+    }
+  }, []);
   return (
     <>
       {/* 메뉴 */}
--- unsu/src/components/integrated/Login.js
+++ unsu/src/components/integrated/Login.js
@@ -1,38 +1,38 @@
 import { useCallback, useState } from "react";
-import { useRecoilState } from "recoil";
-import { loginIdState, loginLevelState } from "../utils/RecoilData";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { loginIdState, loginLevelState, isLoginState } from "../utils/RecoilData";
 import axios from "../utils/CustomAxios";
 import { useNavigate } from "react-router";
 import Jumbotron from "../../Jumbotron";
 
 // 실제 로그인 처리하기 위한 정보 입력 페이지
-const Login = ()=>{
-    
-     //state
-     const [member, setMember] = useState({
-        memberId : "" , memberPw : ""
+const Login = () => {
+
+    //state
+    const [member, setMember] = useState({
+        memberId: "", memberPw: ""
     });
 
     //recoil
     const [loginId, setLoginId] = useRecoilState(loginIdState);
     const [loginLevel, setLoginLevel] = useRecoilState(loginLevelState);
-
+    
     //callback
-    const changeMember = useCallback(e=>{
+    const changeMember = useCallback(e => {
         setMember({
             ...member,
-            [e.target.name] : e.target.value
+            [e.target.name]: e.target.value
         });
     }, [member]);
 
     //navigator
     const navigator = useNavigate();
 
-    const login = useCallback(async (e)=>{
+    const login = useCallback(async (e) => {
         e.preventDefault(); // 폼 제출의 기본 동작인 페이지 새로고침을 방지
 
-        if(member.memberId.length === 0) return;
-        if(member.memberPw.length === 0) return;
+        if (member.memberId.length === 0) return;
+        if (member.memberPw.length === 0) return;
 
         const resp = await axios.post("/member/login", member);
         console.log(resp.data);//memberId, memberLevel이 있음
@@ -49,29 +49,29 @@ const Login = ()=>{
         navigator("/");
     }, [member]);
 
-    return(
+    return (
         <>
-        <form onSubmit={e=>login(e)}>
-            <Jumbotron title="진짜 로그인"/>
-            <div className="row mt-4">
-                <div className="col">
-                    <label>아이디</label>
-                    <input type="text" name="memberId" className="form-control"
-                        value={member.memberId} onChange={e=>changeMember(e)}/> 
+            <form onSubmit={e => login(e)}>
+                <Jumbotron title="진짜 로그인" />
+                <div className="row mt-4">
+                    <div className="col">
+                        <label>아이디</label>
+                        <input type="text" name="memberId" className="form-control"
+                            value={member.memberId} onChange={e => changeMember(e)} />
+                    </div>
                 </div>
-            </div>
-            <div className="row mt-4">
-                <div className="col">
-                    <label>비밀번호</label>
-                    <input type="text" name="memberPw" className="form-control"
-                        value={member.memberPw} onChange={e=>changeMember(e)}/> 
+                <div className="row mt-4">
+                    <div className="col">
+                        <label>비밀번호</label>
+                        <input type="text" name="memberPw" className="form-control"
+                            value={member.memberPw} onChange={e => changeMember(e)} />
+                    </div>
                 </div>
-            </div>
-            <div className="row mt-4">
-                <div className="col">
-                    <button type="submit" className="btn btn-success w-100">로그인</button>
+                <div className="row mt-4">
+                    <div className="col">
+                        <button type="submit" className="btn btn-success w-100">로그인</button>
+                    </div>
                 </div>
-            </div>
             </form>
         </>
     );
