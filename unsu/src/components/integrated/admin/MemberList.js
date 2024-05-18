62cca9f10c8c87436268417879ab8fe431ef77b5
lovejr1114 <lovejr1114@naver.com>
2024-05-19 01:27:12 +0900

    회원검색완료, 예약 설명 추가

--- unsu/src/components/integrated/admin/MemberList.js
+++ unsu/src/components/integrated/admin/MemberList.js
@@ -6,49 +6,93 @@ import { Modal } from "bootstrap";
 import Pagination from '../../utils/Pagination';
 import { RiUserSearchFill } from "react-icons/ri";
 
-
-
 const MemberList = () => {
     //state
     const [members, setMembers] = useState([]);
     const [selectMember, setSelectMember] = useState({
-        memberId:"",
-        memberName:"",
-        memberBirth:"",
-        memberEmail:"",
-        memberPhone:"",
-        memberLevel:"",
-        memberJoinDate:"",
-        memberLoginDate:"",
-        memberPoint:"",
-        memberZip:"",
-        memberAddr1:"",
-        memberAddr2:"",
-        memberPrivacyAgree:"",
-        memberPrivacyDate:"",
-        memberServiceAgree:"",
-        memberBusAgree:""
+        memberId: "",
+        memberName: "",
+        memberBirth: "",
+        memberEmail: "",
+        memberPhone: "",
+        memberLevel: "",
+        memberJoinDate: "",
+        memberLoginDate: "",
+        memberPoint: "",
+        memberZip: "",
+        memberAddr1: "",
+        memberAddr2: "",
+        memberPrivacyAgree: "",
+        memberPrivacyDate: "",
+        memberServiceAgree: "",
+        memberBusAgree: ""
     });
     //페이징
     const [currentPage, setCurrentPage] = useState(1);
     const [postsPerPage, setPostsPerPage] = useState(10);
     //검색
+    const [defaultColumn, setDefaultColumn] = useState("member_id");//기본값
     const [keyword, setKeyword] = useState(""); // 검색어는 빈 문자열로 초기화
-
+    const [searched, setSearched] = useState(false); //검색 여부 상태 추가
 
     //불러와
     useEffect(() => {
         loadData();
     }, []);
+
     //콜백해
     const loadData = useCallback(async () => {
-        const resp = await axios.get("/member/");
-        setMembers(resp.data);
-    }, [members]);
+        try {
+            const resp = await axios.get("/member/");
+            if (Array.isArray(resp.data)) {
+                setMembers(resp.data);
+            } else {
+                console.error("응답 데이터가 배열이 아닙니다:", resp.data);
+                setMembers([]);
+            }
+        } catch (error) {
+            console.error("회원 목록을 불러오는 중 오류 발생:", error);
+            setMembers([]);
+        }
+    }, []);
 
     useEffect(() => {
         setPostsPerPage(10);
     }, [members]);
+    useEffect(()=>{
+        handleSearch();
+    },[searched])
+
+    //검색핸들
+    const handleSearch = (async () => {
+        try {
+            console.log("column : " + defaultColumn);
+            console.log("keyword : " + keyword);
+            const resp = await axios.get(`/member/search/column/${defaultColumn}/keyword/${keyword}`);
+            if (Array.isArray(resp.data)) {
+                // 검색 결과를 받아온 후에 직접 표시할 목록을 업데이트
+                setCurrentPage(1); // 검색 결과는 첫 번째 페이지부터 표시되어야 함
+                setMembers(resp.data);
+            } else {
+                console.error("검색 응답 데이터가 배열이 아닙니다:", resp.data);
+                setMembers([]);
+            }
+            setSearched(true);
+        } catch (error) {
+            console.error("회원 검색 중 오류 발생:", error);
+            setMembers([]);
+        }
+    });
+
+
+    //키워드바꾸기
+    const keywordChange = (e) => {
+        setKeyword(e.target.value);
+    };
+    //컬럼체인지
+    const columnChange = (e) => {
+        setDefaultColumn(e.target.value);
+    };
 
     //모달세트
     const bsModal = useRef(); //상세모달을 위한거
@@ -68,19 +112,20 @@ const MemberList = () => {
     const indexOfFirstPost = indexOfLastPost - postsPerPage;
     const currentPost = members.slice(indexOfFirstPost, indexOfLastPost);
 
-
     return (
         <>
             <Jumbotron title="회원 목록" />
 
             <div className="row mt-4">
-                <div className="col d-flex justify-content-between">
-                    <select className="form-select rounded" style={{width:'15%'}}>
-
+                <div className="col d-flex justify-content-end">
+                    <select className="form-select rounded" style={{ width: '12%' }} onChange={columnChange}>
+                        <option value="member_id">ID</option>
+                        <option value="member_name">이름</option>
+                        <option value="member_level">등급</option>
                     </select>
-                    <input className="form-control rounded text-end" style={{width:'30%'}}/>
-                    <button className="btn btn-outline">
-                        <RiUserSearchFill/>
+                    <input className="ms-2 form-control rounded" style={{ width: '25%' }} onChange={keywordChange} />
+                    <button className="ms-2 btn btn-outline-primary" onClick={handleSearch}>
+                        <RiUserSearchFill />
                     </button>
                 </div>
             </div>
@@ -98,24 +143,29 @@ const MemberList = () => {
                             </tr>
                         </thead>
                         <tbody>
-                            {currentPost.map(member => (
+                        {currentPost.length > 0 ? (
+                                currentPost.map((member, index) => (
+                                    <tr key={index}>
+                                        <td>{member.memberId}</td>
+                                        <td>{member.memberName}</td>
+                                        <td>{member.memberLevel}</td>
+                                        <td>{member.memberEmail}</td>
+                                        <td>
+                                            <FaUserShield onClick={e => openModalInfo(member)}
+                                                style={{ cursor: 'pointer' }}
+                                                className="text-primary" />
+                                        </td>
+                                    </tr>
+                                ))
+                            ) : (
                                 <tr>
-                                    <td>{member.memberId}</td>
-                                    <td>{member.memberName}</td>
-                                    <td>{member.memberLevel}</td>
-                                    <td>{member.memberEmail}</td>
-                                    <td>
-                                        <FaUserShield onClick={e => openModalInfo(member)}
-                                            style={{ cursor: 'pointer' }} 
-                                            className="text-primary"/>
-                                    </td>
+                                    <td colSpan="5">회원 정보를 찾을 수 없습니다.</td>
                                 </tr>
-                            ))}
+                            )}
                         </tbody>
                     </table>
                 </div>
             </div>
-
 
             {/* Pagination */}
             <Pagination
@@ -125,10 +175,8 @@ const MemberList = () => {
                 currentPage={currentPage}
             />
 
-
-
             {/* 상세보기 모달 */}
-            <div ref={bsModal} className="modal fade" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div ref={bsModal} className="modal fade" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
                 <div className="modal-dialog">
                     <div className="modal-content">
                         <div className="modal-header">
@@ -136,7 +184,6 @@ const MemberList = () => {
                             <button type="button" className="btn-close" aria-label="Close" onClick={e => closeModalInfo()}></button>
                         </div>
                         <div className="modal-body">
-                            
                             <div className="row mt-4">
                                 <div className="col">
                                     <label>ID</label>
